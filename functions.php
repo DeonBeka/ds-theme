7d5b0117bbc1219998d6567269688c5bdc04db5c
DeonBeka <deonbeka92@gmail.com>
2025-11-19 19:16:19 +0100

    ./.

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function ds_style(){
     wp_enqueue_style('digitalschool-style', get_stylesheet_uri());
+    wp_enqueue_style('style', get_template_directory_uri().'/css/bootstrap.min.css' , false, '1.0' , 'all');
 }
 add_action('wp_enqueue_scripts', 'ds_style');
 
@@ -12,6 +13,14 @@ function digitalschool_setup(){
 }
 add_action('after_setup_theme', 'digitalschool_setup');
 
+add_action('wp_enqueue_scripts', function(){
+    if(is_page_template('aboutus.php')){
+        wp_enqueue_script(
+            'tailwind-play',
+            'https://cdn.tailwindcss.com'
+        );
+    }
+});
 function ds_js(){
     wp_enqueue_script('ds-js', get_theme_file_uri('js/main.js'), array(), 1.0, true);
 }
